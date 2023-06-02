2e3c490fb7cae9234dbd642242447964ff2ea499
Martin Tasch <mart.tasch@gmail.com>
2023-06-02 16:23:06 +0200

    Icons for TabBar

--- App.js
+++ App.js
@@ -10,6 +10,8 @@ import ProjectScreen from "./screens/ProjectScreen";
 
 const Tab = createBottomTabNavigator();
 
+import { FontAwesome5, MaterialCommunityIcons  } from '@expo/vector-icons';
+
 const Empty = () => {
   return (
     <View />
@@ -20,10 +22,34 @@ export default function App() {
   return (
     <NavigationContainer>
       <Tab.Navigator>
-        <Tab.Screen name = "TaskScreen" component={TaskScreen} /> 
+        <Tab.Screen
+          name = "TaskScreen"
+          component={TaskScreen}
+          options={{
+            tabBarLabel: 'Tasks',
+            tabBarIcon: ({ color, size }) => (
+              <FontAwesome5 style={{ alignSelf: 'center' }} name={'tasks'} size={32} color={color} /> 
+            ),
+           }} /> 
         {/*<Tab.Screen name="NewTask" component={NewEditTaskScreen} /*/}
-        <Tab.Screen name="Calendar" component={CalendarScreen} />
-        <Tab.Screen name="ProjectScreen" component={ProjectScreen} />
+        <Tab.Screen
+          name="Calendar"
+          component={CalendarScreen}
+          options={{
+            tabBarLabel: 'Calendar',
+            tabBarIcon: ({ color, size }) => (
+              <FontAwesome5 style={{ alignSelf: 'center' }} name={'calendar-check'} size={32} color={color} /> 
+            ),
+           }} />
+        <Tab.Screen
+          name="ProjectScreen"
+          component={ProjectScreen}
+          options={{
+            tabBarLabel: 'Projects',
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons style={{ alignSelf: 'center' }} name={'bookshelf'} size={32} color={color} /> 
+            ),
+           }} />
       </Tab.Navigator>
     </NavigationContainer>
   );
